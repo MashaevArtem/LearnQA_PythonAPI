16c8c360cf2aaf73c3acde64be09f9a89d08774c
amashaev <amashaev@inttrust.ru>
2024-03-25 18:37:09 +0300

    get запрос 1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# import requests
-#
-# response = requests.get("https://playground.learnqa.ru/api/hello")
-# print(response.text)
-print("Hello from Artem")
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
+# print("Hello from Artem")
